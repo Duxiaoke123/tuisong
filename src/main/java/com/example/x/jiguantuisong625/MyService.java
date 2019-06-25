585fd066eb79d17d47f9e9e10ec85311f66b5151
Duxiaoke123 <48478328+Duxiaoke123@users.noreply.github.com>
2019-06-25 21:34:41 +0800

    first commit

---
+++ src/main/java/com/example/x/jiguantuisong625/MyService.java
@@ -0,0 +1,12 @@
+package com.example.x.jiguantuisong625;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * @Author：x
+ * @E-mail：
+ * @Date：2019/6/25 21:13
+ * @Description：描述信息
+ */
+public class MyService extends JCommonService {
+}
